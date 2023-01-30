9352b7f30b14cc6fd57e7635fa437bc44000a877
AvantiDharme <122555335+AvantiDharme@users.noreply.github.com>
2023-01-30 12:18:39 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+function updateScore () {
+    OLED.clear()
+    OLED.writeStringNewLine("Player A:" + PA)
+    OLED.newLine()
+    OLED.writeStringNewLine("Player B:" + PB)
+    OLED.newLine()
+    OLED.writeStringNewLine("Ties" + Ties)
+    OLED.newLine()
+    OLED.writeStringNewLine("Rounds" + Rounds)
+}
+let Rounds = 0
+let Ties = 0
+let PB = 0
+let PA = 0
+OLED.init(128, 64)
+PA = 0
+PB = 0
+Ties = 0
+Rounds = 0
+OLED.writeStringNewLine("LETS PLAY ROCK PAPER SCISSORS ")
+basic.pause(2000)
+updateScore()
+basic.forever(function () {
+	
+})
